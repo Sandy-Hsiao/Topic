207f51fdb53250d17afe3b7f5937c752e36e9e18
Sandy-Hsiao <80087896+Sandy-Hsiao@users.noreply.github.com>
2021-12-15 01:07:07 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://sandy-hsiao.github.io/Topic/model.json';
 
 
 function preload() {
